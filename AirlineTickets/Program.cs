592f499f30026bd18d233878e48a5b4731b3e18d
esysop <leehassel@gmail.com>
2016-04-12 18:15:07 -0500

    Have a working collection, now need to iterate

---
+++ AirlineTickets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineTickets
+	{
+	public class Program
+		{
+
+		static void Main (string[] args)
+			{
+			TicketDesk ticketDesk = new TicketDesk();
+			ticketDesk.addFlights();
+			Console.ReadLine();
+			}
+		
+
+			public Program ()
+			{
+			
+			}
+		
+
+			}
+		}
+	
